fff3ed18ed0b31de091e3173860a7e312231b69b
Tajindersohi <126231353+Tajindersohi@users.noreply.github.com>
2023-11-29 22:06:50 +0530

    Add files via upload

---
+++ Bitwise_Shift_Practice.js
@@ -0,0 +1,29 @@
+    
+    
+    ////// bitwise left shift/////
+
+    let x=12
+
+    //using simple multiplication
+    let y=x*2;
+    // using left shift
+    let z=x<<1
+    
+    //both y and z gives the same result
+    console.log(`value of y is ${y} and value of z is ${z}`);
+
+
+
+    ////bitwise right shift ////////
+
+
+    let a=12
+
+    //using simple division
+    let b=a/2
+
+    //division using left shift
+    let c=a>>1
+  
+    //both y and z gives the same result
+    console.log(`value of b is ${b} and value of c is ${c}`);
